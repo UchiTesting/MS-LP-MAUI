0576b9748ed3ea0aed386fd1dc4084c7f65b5ed2
UchiTesing <uchitesting57@gmail.com>
2022-08-01 15:45:22 +0200

    Add project files.

---
+++ MsLearnMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MsLearnMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MsLearnMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MsLearnMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
