3c0f4fea4c7b3d8c0b33a2416fc4a46a645bc7f6
UchiTesing <uchitesting57@gmail.com>
2022-08-02 16:29:50 +0200

    Created a SharedResources class and used for Label and Editor

---
+++ Notes/SharedResources.cs
@@ -0,0 +1,7 @@
+﻿namespace Notes;
+
+static class SharedResources
+{
+    public static readonly Color FontColor = Color.FromRgb(0, 0, 0xff);
+    public static readonly Color BackgroundColor = Color.FromRgb(0xff, 0xfa, 0xaa);
+}
